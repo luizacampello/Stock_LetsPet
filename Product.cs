5f46470b48be3b80f54d3583751cf1162e604e1a
MatAmaro <106554804+MatAmaro@users.noreply.github.com>
2022-06-21 16:17:53 -0300

    Product antigo

--- Product.cs
+++ Product.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Stock
 {
-    internal class Product
+    public class Product
     {
+        public string BarCode { get; set; }
+        public Services ServiseUse { get; set; }
+        public string Name { get; set; }
+        public TipoProdutos Tipo { get; set; }
+        public bool Special { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public double NetWeight { get; set; }
+        public double NetWeightRest { get; set; }
+        public DateTime ExperationDate { get; set; }
+        public bool OpenProduct { get; set; }
+        public DateTime DataCadastro { get; } = DateTime.Now;
     }
 }
--- ProductInUse.cs
+++ ProductInUse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Stock
 {
-    internal class Product
+    internal class ProductInUse
     {
     }
 }
