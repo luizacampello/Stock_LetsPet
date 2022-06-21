20819e7c406735a4232483419197ba46fb216722
MatAmaro <106554804+MatAmaro@users.noreply.github.com>
2022-06-21 16:47:21 -0300

    Enum e product em progresso

--- Product.cs
+++ Product.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stock
 {
     public class Product
     {
-        public string BarCode { get; set; }
-        public Services ServiseUse { get; set; }
+
+        public Type type { get; set; }
         public string Name { get; set; }
-        public TipoProdutos Tipo { get; set; }
-        public bool Special { get; set; }
         public string Brand { get; set; }
         public decimal Price { get; set; }
-        public double NetWeight { get; set; }
-        public double NetWeightRest { get; set; }
+        public int TotalVolume { get; set; }
+        public int CurrentVolume { get; set; }
         public DateTime ExperationDate { get; set; }
-        public bool OpenProduct { get; set; }
-        public DateTime DataCadastro { get; } = DateTime.Now;
+        public Species Species { get; set; }
+
+
+
     }
 }
