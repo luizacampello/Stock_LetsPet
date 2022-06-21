40289c468f4df097c9f3439029917884f20e68ba
Luiza Campello <lumcampello@gmail.com>
2022-06-21 20:59:46 -0300

    First commit on Stock and ProductRegistration Classes

--- Stock.cs
+++ Stock.cs
@@ -8,5 +8,17 @@ namespace Stock
 {
     internal class Stock
     {
+        private List<Product> StoredShampoo = new();
+        public List<Product> StoredSpecialShampoo = new();
+        public List<Product> StoredConditioner = new();
+        public List<Product> StoredSpecialConditioner = new();
+        public List<Product> StoredPerfume = new();
+        public List<Product> StoredSpecialPerfume = new();
+
+        List<Product> GetFullStock()
+        {
+            
+        }
+        
     }
 }
