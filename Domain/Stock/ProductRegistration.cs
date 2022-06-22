198937a41c9dab8e4f8b794355aab1de8a477041
Luiza Campello <lumcampello@gmail.com>
2022-06-22 16:50:49 -0300

    fixed merge conflicts

--- Domain/Stock/ProductRegistration.cs
+++ Domain/Stock/ProductRegistration.cs
@@ -13,41 +13,28 @@ namespace Stock.Domain.Stock
         private int maxAmount = 20;
 
         void NewProductRegistry()
-        //void newProduct()
-        //{
-        //    newProductType();
-        //    newProductName();
-        //    newProductBrand();
-            
-
-
-
-
-        //}
-        public Type SelectType()
         {
             Product newProduct = new();
-            newProduct.type = newProductType();
+            newProduct.Type = newProductType();
             newProduct.Usage = newProductUsage();
             newProduct.Name = newProductName();
             newProduct.Brand = newProductBrand();
+        }
 
-
+        public Type SelectType()
+        {            
             Console.WriteLine("Qual o tipo de produto: Digite:\n1 para Shampoo\n2 para Condicionador\n3 para Perfume");
             var inputType = Console.ReadLine();
             switch (inputType)
             {
                 case "1":
                     return Type.Shampoo;
-
                    
                 case "2":
                     return Type.Conditioner;
-
                    
                 case "3":
                     return Type.Perfume;
-
                     
                 default:
 
@@ -57,7 +44,6 @@ namespace Stock.Domain.Stock
             }
             return Type.Shampoo;
         }
-
 
     }
 }
