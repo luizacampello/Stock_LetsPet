609be2912f5da8e380c22b70fb29b1639eafaf7f
MatAmaro <106554804+MatAmaro@users.noreply.github.com>
2022-06-22 16:41:42 -0300

    Selecionar type movido para productR

--- Domain/Stock/Product.cs
+++ Domain/Stock/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stock.Domain.Common;
+using System;
 using System.Collections.Generic;
 
 namespace Stock.Domain.Stock
@@ -6,7 +7,7 @@ namespace Stock.Domain.Stock
     public class Product
     {
 
-        public Type Type { get; set; }
+        public Type Type { get; set; }       
         public Usage Usage { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
@@ -16,32 +17,7 @@ namespace Stock.Domain.Stock
         public DateTime ExpirationDate { get; set; }
         public Species Species { get; set; }
 
-        public Type SelectType()
-        {
-            Console.WriteLine("Qual o tipo de produto: Digite:\n1 para Shampoo\n2 para Condicionador\n3 para Perfume");
-            var inputType = Console.ReadLine();
-            switch (inputType)
-            {
-                case "1":
-                    Type = Type.Shampoo;
-
-                    break;
-                case "2":
-                    Type = Type.Conditioner;
-
-                    break;
-                case "3":
-                    Type = Type.Perfume;
-
-                    break;
-                default:
-
-                    Console.WriteLine("Entrada invalida, tente novamente");
-                    SelectType();
-                    break;
-            }
-             return Type;
-        }
+        
     }
 
 
--- Domain/Stock/ProductRegistration.cs
+++ Domain/Stock/ProductRegistration.cs
@@ -8,17 +8,46 @@ namespace Stock.Domain.Stock
 {
     internal class ProductRegistration
     {
+        Product newProduct = new Product();
         private int minAmount = 2;
         private int maxAmount = 20;
 
-        void newProduct()
+        //void newProduct()
+        //{
+        //    newProductType();
+        //    newProductName();
+        //    newProductBrand();
+            
+
+
+
+
+        //}
+        public Type SelectType()
         {
-            newProductType();
-            newProductName();
-            newProductBrand();
+            Console.WriteLine("Qual o tipo de produto: Digite:\n1 para Shampoo\n2 para Condicionador\n3 para Perfume");
+            var inputType = Console.ReadLine();
+            switch (inputType)
+            {
+                case "1":
+                    return Type.Shampoo;
 
+                   
+                case "2":
+                    return Type.Conditioner;
 
+                   
+                case "3":
+                    return Type.Perfume;
 
+                    
+                default:
+
+                    Console.WriteLine("Entrada invalida, tente novamente");
+                    SelectType();
+                    break;
+            }
+            return Type.Shampoo;
         }
 
 
