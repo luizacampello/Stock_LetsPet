9042cccdda6b9d521ffc8e16dd1758e84c64851c
MatAmaro <106554804+MatAmaro@users.noreply.github.com>
2022-06-22 16:26:12 -0300

    Seleciona Tipo

--- Product.cs
+++ Product.cs
@@ -6,22 +6,43 @@ namespace Stock
     public class Product
     {
 
-        public Type type { get; set; }
+        public Type Type { get; set; }
         public Usage Usage { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
         public decimal Price { get; set; }
         public int TotalVolume { get; set; }
-        public int CurrentVolume { get; set; } 
+        public int CurrentVolume { get; set; }
         public DateTime ExpirationDate { get; set; }
         public Species Species { get; set; }
 
-        public static void Main(string[] args)
+        public Type SelectType()
         {
+            Console.WriteLine("Qual o tipo de produto: Digite:\n1 para Shampoo\n2 para Condicionador\n3 para Perfume");
+            var inputType = Console.ReadLine();
+            switch (inputType)
+            {
+                case "1":
+                    Type = Type.Shampoo;
 
+                    break;
+                case "2":
+                    Type = Type.Conditioner;
+
+                    break;
+                case "3":
+                    Type = Type.Perfume;
+
+                    break;
+                default:
+
+                    Console.WriteLine("Entrada invalida, tente novamente");
+                    SelectType();
+                    break;
+            }
+             return Type;
         }
-
     }
 
-    
+
 }
