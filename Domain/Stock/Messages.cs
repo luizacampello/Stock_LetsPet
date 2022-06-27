a5191f0006b7e22dafb3626418960cfc65349bb6
MatAmaro <106554804+MatAmaro@users.noreply.github.com>
2022-06-27 16:51:54 -0300

    Complementação do ProductRegistration

--- Domain/Stock/Messages.cs
+++ Domain/Stock/Messages.cs
@@ -8,12 +8,29 @@ namespace Stock.Domain.Stock
 {
 	public static class Messages
 	{
-		public const string CategorySelectionMenu = "alo";
+		public const string CategorySelectionMenu = @"Selecione a categoria de produto 
+1 - Shampoo
+2 - Condicionador
+3 - Perfume";
 
-		public const string UsageSelectionMenu = "alo";
+		public const string UsageSelectionMenu = @"Qual o tipo de uso do produto: 
+1 - Comum
+2 - Especial";
 
-		public const string formatError = "alo";
+		public const string formatError = "Entrada invalida, tente novamente";
 
+		public const string inputName = "Qual o nome do produto?";
 
+		public const string inputBrand = "Qual a marca do produto?";
+
+		public const string inputPrice = "Qual o preço do produto?";
+
+		public const string inputTotalVolume = "Quantos ml o produto possúi?";
+
+		public const string inputExpirationDate = "Qual a data de validade do produto?";
+
+		public const string SpecieSelectionMenu = @"Para qual tipo de espécie o produto é indicado:
+1 - Cachorro
+2 - Gato";
 	}
 }
