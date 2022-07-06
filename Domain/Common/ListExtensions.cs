47da6e9fedfb12a54d9cfd92531fb5d92c74d40d
Luiza Campello <lumcampello@gmail.com>
2022-07-06 18:04:18 -0300

    add TryPop Method to ListExtensions

--- Domain/Common/ListExtensions.cs
+++ Domain/Common/ListExtensions.cs
@@ -14,5 +14,23 @@ namespace Stock.Domain.Common
             newCollection.RemoveAt(index);
             return itemAtIndex;
         }
+
+        public static bool TryPop<T>(this List<T> newCollection, out T itemAtIndex, int index = -1)
+        {
+            int listSize = newCollection.Count - 1;
+            if (index == -1)
+            {
+                index = listSize;
+            }
+
+            if (index >= 0 && index <= listSize)
+            {
+                itemAtIndex = newCollection[index];
+                newCollection.RemoveAt(index);
+                return true;
+            }
+            itemAtIndex = default;
+            return false;
+        }
     }
 }
